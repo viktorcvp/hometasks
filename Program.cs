0511326c72f8c8149dbae34a9cc3eb457255ebd7
“viktorcvp” <dffggg2013@yandex.by>
2022-09-04 23:14:22 +0300

    m

--- Program.cs
+++ Program.cs
@@ -5,5 +5,8 @@
 //a = -9 b = -3 -> max = -3
 
 
-Console.WriteLine("введите 1 число");
-Console.WriteLine("введите 2 число");
+num 1Console.WriteLine("введите 1 число");
+num 2Console.WriteLine("введите 2 число");
+string? username = Console.ReadLine();
+Console.WriteLine("num 1 + num 2");
+Console.WriteLine(username);
