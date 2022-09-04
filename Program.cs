10f2c13731e06cfe614f0de036d02129a7cf8fb9
“viktorcvp” <dffggg2013@yandex.by>
2022-09-04 23:16:38 +0300

    m

--- Program.cs
+++ Program.cs
@@ -7,6 +7,5 @@
 
 num 1Console.WriteLine("введите 1 число");
 num 2Console.WriteLine("введите 2 число");
-string? username = Console.ReadLine();
-Console.WriteLine("num 1 + num 2");
-Console.WriteLine(username);
+
+
