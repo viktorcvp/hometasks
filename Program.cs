2237654519e357f66b49d52da90c8849946c2446
“viktorcvp” <dffggg2013@yandex.by>
2022-09-08 22:31:13 +0300

    hetnie

--- Program.cs
+++ Program.cs
@@ -58,3 +58,23 @@ void rest()
 
 }
 rest();
+
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+void res()
+{
+    Console.WriteLine("введите  число a");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+
+    for (int i = 2; i < a; i++)
+    {
+        if (i % 2 == 0)
+            Console.Write(i);
+    }
+}
+res();
